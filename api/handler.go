16979318454b44ce9c6251adb18d77ead743327d
Stephanie Cardoso <stephaug12@gmail.com>
2024-05-04 01:45:32 -0300

    Add Swagger

--- api/handler.go
+++ api/handler.go
@@ -11,15 +11,49 @@ import (
 	"gorm.io/gorm"
 )
 
+// getStudents godoc
+//
+//	@Summary        Get a list of students
+//	@Description    Retrieve students details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Param          register path    int  false    "Registration"
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Router         /students [get]
 func (api *API) getStudents(c echo.Context) error {
 	students, err := api.DB.GetStudents()
 	if err != nil {
 		return c.String(http.StatusNotFound, "Failed to get students")
 	}
 
-	return c.JSON(http.StatusOK, students)
+	active := c.QueryParam("active")
+
+	if active != "" {
+		act, err := strconv.ParseBool(active)
+		if err != nil {
+			log.Error().Err(err).Msgf("[api] error to parse boolean")
+			return c.String(http.StatusInternalServerError, "Failed to parse boolean")
+		}
+		students, err = api.DB.GetFilteredStudent(act)
+	}
+
+	listOfStudents := map[string][]schemas.StudentResponse{"students": schemas.NewResponse(students)}
+
+	return c.JSON(http.StatusOK, listOfStudents)
 }
 
+// createStudent godoc
+//
+//	@Summary        Create student
+//	@Description    Create student
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        400
+//	@Router         /students [post]
 func (api *API) createStudent(c echo.Context) error {
 	studentReq := StudentRequest{}
 	if err := c.Bind(&studentReq); err != nil {
@@ -43,9 +77,20 @@ func (api *API) createStudent(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, "Error to create student")
 	}
 
-	return c.String(http.StatusOK, "Create student")
+	return c.JSON(http.StatusOK, student)
 }
 
+// getStudent godoc
+//
+//	@Summary        Get student by ID
+//	@Description    Retrieve student details using ID
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [get]
 func (api *API) getStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -64,6 +109,17 @@ func (api *API) getStudent(c echo.Context) error {
 	return c.JSON(http.StatusOK, student)
 }
 
+// updateStudent godoc
+//
+//	@Summary        Update Student
+//	@Description    Update student details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [put]
 func (api *API) updateStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -93,6 +149,17 @@ func (api *API) updateStudent(c echo.Context) error {
 	return c.JSON(http.StatusOK, student)
 }
 
+// deleteStudent godoc
+//
+//	@Summary        Delete Student
+//	@Description    Delete student details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [delete]
 func (api *API) deleteStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,18 +5,30 @@ go 1.22
 require (
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/rs/zerolog v1.32.0
+	github.com/stephanie-cardoso/students-system v0.0.0-20240503221048-9f988cf3b7f1
+	github.com/swaggo/echo-swagger v1.4.1
+	github.com/swaggo/swag v1.16.3
 	gorm.io/driver/sqlite v1.5.5
 	gorm.io/gorm v1.25.10
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
@@ -24,4 +36,7 @@ require (
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
